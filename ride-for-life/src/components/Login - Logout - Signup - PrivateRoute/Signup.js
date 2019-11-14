43bb25c21079af61b9cbf456714a4680284cc9ea
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-11-13 20:16:51 -0600

    Created some pseudocode for our sign-up form.

---
+++ ride-for-life/src/components/Login - Logout - Signup - PrivateRoute/Signup.js
@@ -0,0 +1,39 @@
+
+
+
+
+// Checkbox - if unchecked, user. If 
+
+const user = {
+    name: "",
+    username: "",
+    password: "",
+    role_id: ""
+}
+
+//This is what will happen when a user signs up. User should choose their role some way.
+
+// switch('role_id') {
+
+//SUPER STRETCH GOAL ________
+//     case('role_id' === 1):
+        // return (<Admin />)
+        // break;
+//___________________________
+
+
+
+//MVP _______________________
+//     case('role_id' === 2):
+        // return (<Driver />)
+        // break;
+
+
+//     case('role_id' === 3):
+        // return (<Rider />)
+        // break;
+
+//      default:
+//          return <something/>
+
+        // }
