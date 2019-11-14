a4149dc7d4706af2de54c1251aad28c6af1fbdb1
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-11-13 19:53:51 -0600

    Added additional components and created an api utility.

--- ride-for-life/src/App.js
+++ ride-for-life/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { axiosWithAuth as axios } from './utils/api'
 
 function App() {
+
+
   return (
     <div className="App">
      <h1>Hello. Welcome to Ride for Life.</h1>
     </div>
+    
   );
 }
 
