ba0e0bdeb6f430c6c34e10b9dba467d24f71f9c6
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-11-14 20:02:25 -0600

    Wrote the PrivateRoute component.

---
+++ ride-for-life/src/components/Login - Logout - Signup - PrivateRoute/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+//Route import
+import { Route, Redirect } from 'react-router-dom'
+import { getToken } from '../../utils/api'
+
+const PrivateRoute = (props) => {
+    
+const {
+    component: Component,
+    ...rest
+} = props
+
+return (
+        <Route {...rest} render={(renderProps) => {
+
+            if(getToken()) {
+                return <Component {...renderProps} />
+            } else {
+                return <Redirect to='/login' />
+            }
+        }} />
+    )
+}
+
+export default PrivateRoute;
