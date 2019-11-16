b1a0184e98a5afa692915b5a8af0186023e655f5
AustinKelsay <austinkelsay@yahoo.com>
2019-11-15 19:24:47 -0600

    Fixed handleSubmit and yup errors bugs, signup form is now fully functional

--- ride-for-life/src/components/Login - Logout - Signup - PrivateRoute/Signup.js
+++ ride-for-life/src/components/Login - Logout - Signup - PrivateRoute/Signup.js
@@ -4,92 +4,77 @@ import {Button} from "reactstrap";
 import * as yup from "yup";
 
 
-const Signup = ({handleSubmit}) => {
+const Signup = ({handleSubmit, errors, touched, values, handleChange}) => {
 
 
         return(
-                <Formik
-                  initialValues={{ name: '',  username: '', password: '', role: false}}
-                  render={props => {
-                          console.log(props)
-                    return(
-                    <div className="form-list">
-
-                      <Form 
-                      className='form' 
-                      onSubmit={handleSubmit}
-                      >
+                <Form 
+                  className='form' 
+                  onSubmit={handleSubmit}
+                  >
                               
-                      <label>Name</label>
-                    <div>
-                        {props.touched.name && props.errors.name && (
-                            <p>Error: {props.errors.name}</p>
-                            )} 
-                            <Field name='name' 
-                            type='text' 
-                            placeholder='enter name' 
-                            value={props.values.name} 
-                            onChange={props.handleChange} />
-                    </div>
-        
-                            <label>Username</label>
-                    <div>
-                        {props.touched.username && props.errors.username && (
-                            <p>Error: {props.errors.username}</p>
-                            )} 
-                            <Field 
-                            name='username' 
-                            type='text' 
-                            placeholder='enter a username' 
-                            value={props.values.username} 
-                            onChange={props.handleChange} />
-                    </div>
-        
-                            <label>Password</label>
-                    <div>
-                        {props.touched.password && props.errors.password && (
-                            <p>Error: {props.errors.password}</p>
-                            )}
-                            <Field 
-                            name='password' 
-                            type='text' 
-                            placeholder='enter a password' 
-                            value={props.values.password} 
-                            onChange={props.handleChange} />
-                    </div>
-        
-                    <div>
-                        {props.touched.role && props.errors.role && (
-                            <p>Error: {props.errors.role}</p>
-                            )}
-                            <label>Are you signing up to be a driver?</label>
-                            <br/>
-                            <Field 
-                            type='checkbox' id='role' 
-                            name='role' 
-                            checked={props.values.role} 
-                            onChange={props.handleChange} />
-                    </div>
-            
+                        <label>Name</label>
+                        <div>
+                                {touched.name && errors.name && (
+                                <p>Error: {errors.name}</p>)} 
+                                <Field name='name' 
+                                type='text' 
+                                placeholder='enter name'     
+                                value={values.name} 
+                                onChange={handleChange} />
+                        </div>
+                
+                        <label>Username</label>
+                        <div>
+                                {touched.username && errors.username && (
+                                <p>Error: {errors.username}</p>)} 
+                                <Field 
+                                name='username' 
+                                type='text' 
+                                placeholder='enter a username' 
+                                value={values.username} 
+                                onChange={handleChange} />
+                        </div>
+                
+                        <label>Password</label>
+                        <div>
+                        {touched.password && errors.password && (
+                                <p>Error: {errors.password}</p>)}
+                                <Field 
+                                name='password' 
+                                type='text' 
+                                placeholder='enter a password' 
+                                value={values.password} 
+                                onChange={handleChange} />
+                        </div>
+                
+                        <div>
+                                {touched.role && errors.role && (
+                                <p>Error: {errors.role}</p>)}
+                                <label>Are you signing up to be a driver?</label>
+                                <br/>
+                                <Field 
+                                type='checkbox' id='role' 
+                                name='role' 
+                                checked={values.role} 
+                                onChange={handleChange} />
+                        </div>
+                
                         <Button outline color="primary" className='submit' type="submit" >Submit</Button>
-                      </Form>                  
-                      </div>
-                    )
-                  }}
-                />
+                </Form>                  
               );
-        
+              
         }
         const formikUserForm = withFormik({
-            mapPropsToValues({name, username, password, role}){
-                console.log()
-                return {
-                  name: name || "",
-                  username: username || "",
-                  password: password ||  "",
-                  role: role
+            mapPropsToValues({name, username, password, role, errors, values}){
+                    console.log(values)
+                    return {
+                            name: name || "",
+                            username: username || "",
+                            password: password ||  "",
+                            role: role || false
                 };
-            },
+        },
               validationSchema: yup.object().shape({
                   name: yup.string()
                   .required()
@@ -107,8 +92,7 @@ const Signup = ({handleSubmit}) => {
                   role: yup.bool()
              }),
             handleSubmit:(values, {}) => {
-                    
-                console.log(values)
+                    console.log(values)
                 
             }       
         })(Signup);
@@ -117,7 +101,7 @@ const Signup = ({handleSubmit}) => {
 // Checkbox - if unchecked, user. If 
 
 
-export default Signup;
+export default formikUserForm;
 
 //This is what will happen when a user signs up. User should choose their role some way.
 
